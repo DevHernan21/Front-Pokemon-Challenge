58397d23798cebc9df9c471bb1b69b3075335b53
Hernan Castro <hcastro0051@gmail.com>
2022-12-07 11:38:23 -0300

    se cambia la baseURL

--- src/apiConfig/apiAxiosInstance.ts
+++ src/apiConfig/apiAxiosInstance.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const apiAxiosInstance = axios.create({
-  // baseURL: "https://pokeapi.co/api/v2/pokemon/",
-  baseURL: "http://localhost:8080/pokemon/",
+  baseURL: "https://pokemonchallengeapiserver.azurewebsites.net",
 });
 
 export default apiAxiosInstance;
