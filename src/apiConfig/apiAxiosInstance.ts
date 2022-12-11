1c307f688154bf21e408c0fb650b3389feebabe5
Hernan Castro <hcastro0051@gmail.com>
2022-12-11 12:30:28 -0300

    pruebas locales

--- src/apiConfig/apiAxiosInstance.ts
+++ src/apiConfig/apiAxiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiAxiosGetAll = axios.create({
-  baseURL: "https://pokemonchallengeapiserver.azurewebsites.net/",
+  baseURL: "http://localhost:8080/pokemon/",
 });
 
 export default apiAxiosGetAll;
